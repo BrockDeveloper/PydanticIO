d6d57c3fa9703cb983141d2e3c9e02b23819c80e
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2022-12-27 22:36:23 +0100

    perf: :zap:  pydantic support, removed dict support

--- pyjsonio/jsonio.py
+++ pyjsonio/jsonio.py
@@ -7,7 +7,7 @@ class JsonIO:
 
 
 
-    def __init__(self, json_file: str, model: BaseModel = None) -> None:
+    def __init__(self, json_file: str, model: BaseModel) -> None:
 
         '''
         Initialize the JsonIO class
@@ -19,164 +19,67 @@ class JsonIO:
         '''
 
         self.model = model
-        self.switch = False
         self.file = json_file
 
         with open(json_file, "r") as f:
-            self._dict = json.load(f)
+            _dict = json.load(f)
             f.close()
 
-
-
-    def validate(self) -> bool:
-
-        '''
-        Validate the json file with the pydantic model
-        
-        :return: True if the json file is valid, False if not
-        '''
-
-        for element in self._dict:
-            try:
-                self.model(**element)
-            except:
-                return False
-
-        return True
-
+        self.items = [self.model(**element) for element in _dict]
+        del _dict
+    
 
 
     def read(self, json_file: str) -> None:
 
-        '''
-        Read a json file
-        
-        :param json_file: The json file to read from
-        
-        :return: None
-        '''
-
         with open(json_file, "r") as f:
-            self.file = json_file
-            self._dict = json.load(f)
+            _dict = json.load(f)
             f.close()
 
+        self.items = [self.model(**element) for element in _dict]
+        del _dict
 
-
+    
     def write(self, json_file: str = None) -> None:
-
-        '''
-        Write the json file
-        
-        :param json_file: The json file to write to, if None the file from the constructor is used
-        
-        :return: None
-        '''
 
         if json_file is None:
             json_file = self.file
 
         with open(json_file, "w") as f:
-            json.dump(self._dict, f, indent=4)
+            json.dump([element.dict() for element in self.items], f, indent=4)
             f.close()
 
 
+    def dict(self) -> list:
+        return [element.dict() for element in self.items]
 
-    def dict(self) -> dict:
+    def append(self, item: BaseModel) -> None:
+        self.items.append(item)
 
-        '''
-        Get the json file as a dict
+    def remove(self, item: BaseModel) -> None:
+        self.items.remove(item)
+    
+    def remove(self, index: int) -> None:
+        del self.items[index]
 
-        :return: The json file as a dict
-        '''
 
-        return self._dict
+    def __getitem__(self, key):
+        return self.items[key]
+
+
+    def __iter__(self):    # remove an item by index
+        return iter(self.items)
 
     
-    def json(self) -> str:
-
-        '''
-        Get the json file as a string
-
-        :return: The json file as a string
-        '''
-
-        return json.dumps(self._dict)
+    def __len__(self):
+        return len(self.items)
 
 
-
-    def pydantic(self) -> list:
-
-        '''
-        Get the json file as a list of pydantic objects
-
-        :return: The json file as a list of pydantic objects
-        '''
-
-        return [self.model(**element) for element in self._dict]
-
-
+    def __str__(self):
+        return str(self.items)
     
-    def __len__(self) -> int:
-
-        '''
-        Get the length of the json file
-
-        :return: The length of the json file
-        '''
-
-        return len(self._dict)
-
-    
-
-    def switch_to_pydantic(self, switch: bool) -> None:
-
-        '''
-        Switch between dict and pydantic objects
-
-        :param switch: True to switch to pydantic objects, False to switch to dicts
-
-        :return: None
-        '''
-
-        self.switch = switch
-
-
-
-    def __getitem__(self, key: int) -> dict:
-        
-        '''
-        Get an item from the json file
-
-        :param key: The key of the item to get
-
-        :return: The item as a dict or pydantic object
-        '''
-
-        if self.switch:
-            return self.model(**self._dict[key])
-        else:
-            return self._dict[key]
-
-    
-
-    def __str__(self) -> str:
-
-        '''
-        Get the json file as a string
-
-        :return: The json file as a string
-        '''
-
-        return self.json()
-
-     
-    def __del__(self) -> None:
-
-        '''
-        Delete the object
-
-        :return: None
-        '''
-        
+    def __del__(self):
+        del self.items
+        del self.model
+        del self.file
         del self
