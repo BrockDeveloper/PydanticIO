5b6e5c06f90b89bcdec00c66b1c2b772ce543f07
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2022-12-27 19:47:41 +0100

    feat: :sparkles: io on file, test, basic methods

--- pyjsonio/jsonio.py
+++ pyjsonio/jsonio.py
@@ -6,9 +6,27 @@ class JsonIO:
 
     def __init__(self, json_file: str) -> None:
         
+        self.file = json_file
+
         with open(json_file, "r") as f:
             self._dict = json.load(f)
             f.close()
+
+
+    def read(self, json_file: str) -> None:
+        with open(json_file, "r") as f:
+            self.file = json_file
+            self._dict = json.load(f)
+            f.close()
+
+
+    def write(self, json_file: str = None) -> None:
+        if json_file is None:
+            json_file = self.file
+        with open(json_file, "w") as f:
+            json.dump(self._dict, f, indent=4)
+            f.close()
+
 
 
     def dict(self) -> dict:
@@ -21,3 +39,11 @@ class JsonIO:
     
     def __getitem__(self, key: int) -> dict:
         return self._dict[key]
+
+    
+    def __str__(self) -> str:
+        return self.json()
+
+     
+    def __del__(self) -> None:
+        del self
--- test.py
+++ test.py
@@ -2,6 +2,35 @@ from pyjsonio.jsonio import JsonIO
 
 json_test = JsonIO("testfile.json")
 
+
+print("Basic test:")
 print("Dict: ", json_test.dict())
 print("Json: ", json_test.json())
 print("Element position zero: ", json_test[0])
+
+
+print("Change value in element position zero:")
+print("Before: ", json_test[0])
+json_test[0]["name"] = "Doo"
+print("After: ", json_test[0])
+
+
+print("Write modified json to new file: ")
+json_test.write("testfile2.json")
+print("ok")
+
+
+print("Read new file: ")
+json_test.read("testfile2.json")
+print("Dict: ", json_test.dict())
+
+
+print("test string: ", json_test)
+
+
+print("Delete object: ")
+del json_test
+try:
+    print(json_test)
+except NameError:
+    print("ok")
