5d63ff5016c0e4ae9afb52a338bd26868c6e7d85
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2022-12-29 10:55:23 +0100

    test: :fire: removed test files

--- testmodel.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class TestModel(BaseModel):
-    id: int
-    name: str
-
-    # support item assignment
-    def setattribute(self, key, value):
-        self.__setattr__(key, value)
